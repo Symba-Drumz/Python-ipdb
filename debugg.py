78ab4f817f4f6bc973ceb9a8ba3049964c9af5f8
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-11 13:07:05 +0300

    fix: ipdb successfully applied, revealed bug and removed

--- debugg.py
+++ debugg.py
@@ -3,11 +3,11 @@ import ipdb
  
 
 def calculate_average(scores):
-    ipdb.set_trace() 
+     
     total = 0
     for score in scores:
         total += score
-    average = total / len(score)  
+    average = total / len(scores)  
     return average  
 
 scores = [90, 80, 85, 70]  
